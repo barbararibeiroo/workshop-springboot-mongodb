1ae2d0b5e8db200da6845e8a1f9ebad1acdd1ede
Bárbara Ribeiro <barbararibeiro18@gmail.com>
2020-10-31 02:07:13 -0300

    Endpoint parar retornar os posts de um usuário

--- src/main/java/com/barbararibeiro/workshopmongo/resources/UsuarioResource.java
+++ src/main/java/com/barbararibeiro/workshopmongo/resources/UsuarioResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.barbararibeiro.workshopmongo.DTO.UsuarioDTO;
+import com.barbararibeiro.workshopmongo.domain.Post;
 import com.barbararibeiro.workshopmongo.domain.Usuario;
 import com.barbararibeiro.workshopmongo.servico.UsuarioServico;
 
@@ -57,5 +58,11 @@ public class UsuarioResource {
 	obj.setId(id);
 	obj = servico.update(obj);
 	return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		Usuario obj = servico.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
