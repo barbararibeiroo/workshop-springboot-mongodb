ab81e8afc0b46766c7174734418e69d7cd585aed
Bárbara Ribeiro <barbararibeiro18@gmail.com>
2020-11-01 14:57:48 -0300

    Obtendo um  post por id

---
+++ src/main/java/com/barbararibeiro/workshopmongo/servico/PostServico.java
@@ -0,0 +1,23 @@
+package com.barbararibeiro.workshopmongo.servico;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.barbararibeiro.workshopmongo.domain.Post;
+import com.barbararibeiro.workshopmongo.repositorio.PostRepositorio;
+import com.barbararibeiro.workshopmongo.servico.exception.ObjectNotFoundException;
+
+@Service
+public class PostServico {
+
+	@Autowired
+	private PostRepositorio repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
